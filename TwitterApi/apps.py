6a2ccb55febfe41f17c42a0831a0e453b64bd519
Reetik@ <42797666+reetika-goel@users.noreply.github.com>
2019-11-14 11:59:28 -0800

    Disable the api keys for security reason

--- TwitterApi/apps.py
+++ TwitterApi/apps.py
@@ -1,5 +1,5 @@
 """
-@Authors: Vidhi Shah, Sithara Krishna Murthy
+@Authors: Reetika Goel, Sithara Krishna Murthy
 @Purpose: It contains Twitter API developer keys
 """
 
@@ -7,7 +7,7 @@ from django.apps import AppConfig
 
 class TwitterapiConfig(AppConfig):
     name = 'TwitterApi'
-    consumer_key = "oE5oJ3jDwGSHadbN4BK4yNYuy"
-    consumer_secret = "nmlzHoqkt0tw9hLpFbWWgSzDsEYlfPeVmMlOeybHN84hmJxarn"
-    access_token = "1046137456301506560-4RbRVcUt2FChkrljWF0jD06ikZyaqQ"
-    access_token_secret = "l508SIOvH01kdNSuXTzmwANoZlooDJA66ndRJ8Let5nMK"
+    consumer_key = "******************"
+    consumer_secret = "*****************************************"    # hide all the keys becoz of security reasons
+    access_token = "*********************************************"
+    access_token_secret = "*******************************************"
